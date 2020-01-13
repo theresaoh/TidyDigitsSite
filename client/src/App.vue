156e0f95fb70898eefdb6616120d009c94915bbf
Theresa O'Hare <theresa.ohare@outlook.com>
2020-01-12 20:33:51 -0700

    imported components, added some minor styling for homepage

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <Home msg="TidyDigits Homepage"/>
+    <NavBar></NavBar>
+    <span class="img"></span>
+    <router-view></router-view>
   </div>
 </template>
-
 <script>
+import NavBar from './components/NavBar.vue'
 import Home from './components/Home.vue'
-
+import Technology from './components/Technology.vue'
+import Products from './components/Products.vue'
 export default {
   name: 'app',
   components: {
-    Home
+    NavBar,
+    Home,
+    Technology,
+    Products
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.img {
+  display: block;
+  z-index: 1;
+  width: 100vw;
+  height: 100vh;
+  background-image: url("https://img.wallpapersafari.com/desktop/1920/1080/57/70/gKDlOv.jpg");
+  background-repeat: no-repeat;
+  background-size: 100vw 100vh
+}
+.component {
+  margin: 0 20%;
 }
 </style>
