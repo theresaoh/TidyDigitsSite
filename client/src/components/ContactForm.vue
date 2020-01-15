dad296faeeea909c05b27ef329fe4036741bdc9c
Theresa O'Hare <theresa.ohare@outlook.com>
2020-01-14 21:11:01 -0700

    Basic contact form frame and minor styling

--- client/src/components/ContactForm.vue
+++ client/src/components/ContactForm.vue
@@ -7,7 +7,33 @@
       <div class="modal-content" ref="modalContent">
         <div class="modal-body">
           <span @click="$refs.modal.style.display='none'" class="close">&times;</span>
-          <p>Some text! Neat! </p>
+          <p class="label">Name:</p>
+          <div class="info-container">
+            <span>
+              <input v-model="firstName" type="text" @keyup.enter="checkValidity()"/>
+              <p class="sub-label">First</p>
+            </span>
+            <span>
+              <input v-model="lastName" type="text" @keyup.enter="checkValidity()"/>
+              <p class="sub-label">Last</p>
+            </span>
+          </div>
+          <p class="label">Contact Info:</p>
+          <div class="info-container">
+            <span>
+              <input v-model="email" type="email" @keyup.enter="checkValidity()"/>
+              <p class="sub-label">Email</p>
+            </span>
+            <span>
+              <input v-model="phone" type="text" @keyup.enter="checkValidity()"/>
+              <p class="sub-label">Phone</p>
+            </span>
+          </div>
+          <div class="message-container">
+            <p class="label">Message:</p>
+            <textarea></textarea>
+          </div>
+          <button @click="checkValidity()">SUBMIT</button>
         </div>
       </div>
     </div>
@@ -17,18 +43,60 @@
 <script>
 export default {
   name: 'ContactForm',
+  data(){
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      phone: '',
+      textBody: ''
+    }
+  },
+  methods:{
+    submitContact() {
+      console.log("contact form submitted");
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.label {
+  text-align: left;
+  font-weight: bold;
+}
+
+.sub-label {
+  text-align: left;
+  font-weight: normal;
+}
+
 .button-container {
   position: absolute;
   right: 0;
   top: 50%;
 }
+.info-container {
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 15px 0;
+}
 
-.contactButton {
+textarea {
+  width: 100%;
+  height: 80px;
+  resize: none;
+}
+
+.modal-body {
+  padding: 15px 0;
+  width: 50%;
+  height: 100%;
+}
+
+button {
   font-size: 18px;  
   border: none;
   outline: none;
@@ -36,6 +104,9 @@ export default {
   padding: 14px 16px;
   background-color: #532d9d;
   font-family: inherit;
+}
+
+.contactButton {
   margin: 0;
   position: fixed;
   z-index: 100;
@@ -79,6 +150,7 @@ a {
 
 /* Modal Content/Box */
 .modal-content {
+  border-radius: 10px;
   position: relative;
   display: flex;
   justify-content: center;
@@ -87,7 +159,7 @@ a {
   margin: auto;
   padding: 2px 16px;  
   border: 1px solid #888;
-  width: 20%;
+  width: 40%;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19);
   animation-name: animatedown;
   animation-duration: 0.5s
