f6c879cbb7d8bb6c49c306badd90b022e7444b48
Theresa O'Hare <theresa.ohare@outlook.com>
2020-01-15 21:01:25 -0700

    basic email handling file added

---
+++ server/emailHandler.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+import os
+
+port = 465  # For SSL
+password = os.environ["EMAIL_PASSWORD"]
+email = os.environ["EMAIL_ADDRESS"]
+
+sender_email = "no-reply@adomain.com"
+message = (f'From: {sender_email}\nTo: {email}\nSubject: Hi There\n\nThis is a message sent with Python! How neat is that?!')
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(email, password)
+    server.sendmail(sender_email, email, message)
