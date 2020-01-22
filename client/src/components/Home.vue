2fdd6a260f641f85bce7e84d7f3495059ae10999
Theresa O'Hare <theresa.ohare@outlook.com>
2020-01-22 00:02:35 -0700

    added floating textbox over image and removed boilerplate styling

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="component">
-    <span class="img"></span>
+    <span class="img">
+      <div class="text-block">
+        <p class="heading">Weather and Climate Data Products</p>
+      </div>
+    </span>
     <div class="home">
       <p class="heading">TidyDigits</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -26,20 +30,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 .img {
   display: block;
   z-index: 1;
@@ -49,13 +40,27 @@ a {
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
+  display: flex;
+  justify-content: center;
 }
-h1{
-  color: rgb(225, 230, 234);
-}
+
 .home {
   margin: 40px 30% 40px 30%;
   line-height: 2.5;
   font-weight: 300; 
+}
+.text-block {
+  z-index: 5;
+  border: 2px solid rgb(44, 62, 80);
+  border-radius: 2px;
+  background-color: rgb(125, 156, 151, 0.8);
+  color: white;
+  align-self: center;
+}
+
+.heading {
+  font-size: 25px;
+  padding: 20px;
+  margin: 0;
 }
 </style>
