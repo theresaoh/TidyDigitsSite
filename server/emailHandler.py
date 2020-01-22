385de3c333bef3705694ee7e91c9c0405a48726e
Theresa O'Hare <theresa.ohare@outlook.com>
2020-01-21 18:55:45 -0700

    Email now being sent to admin on successful contact form submission with contact form info

--- server/emailHandler.py
+++ server/emailHandler.py
@@ -22,41 +22,51 @@ def test_duplicate_user():
   email_to_user["To"] = receiver_email
 
   # Create the plain-text and HTML version of your message
-  text = f"""
-  Hello {first_name},
-  Thank you for contacting TidyDigits. We received your messsage and will be reaching out to you within 5-7 business days. 
-  Message you submitted:
-  {submitted_message}
+  email_to_user_plain_text = f"""
+    Hello {first_name},
+    Thank you for contacting TidyDigits. We received your messsage and will be reaching out to you within 5-7 business days. 
+    Message you submitted:
+    {submitted_message}
 
-  We look forward to doing business with you!
-  TidyDigits Team
+    We look forward to doing business with you!
+    TidyDigits Team
   """
-  html = f"""
-  <html>
-  <head>
-    <style>
-    body {{text-align: center; font-family: 'Avenir', Helvetica, Arial, sans-serif; }}
-    .container {{padding: 20px; border-radius: 10px;}}
-    p {{font-size: 14px;}}
-    </style>
-  </head>
-    <body>
-      <div class="container">
-        <h1>Hello {first_name},</h1><br>
-        <p>Thank you for contacting TidyDigits. We received your messsage and will be reaching out to you within 5-7 business days.<br>
-          Message you submitted:<br>
-          {submitted_message}<br><br>
-          We look forward to doing business with you!<br>
-          TidyDigits Team
-        </p>
-      </div>
-    </body>
-  </html>
+
+  email_to_user_html = f"""
+    <html>
+    <head>
+      <style>
+      body {{text-align: center; font-family: 'Avenir', Helvetica, Arial, sans-serif; }}
+      .container {{padding: 20px; border-radius: 10px;}}
+      p {{font-size: 14px;}}
+      </style>
+    </head>
+      <body>
+        <div class="container">
+          <h1>Hello {first_name},</h1><br>
+          <p>Thank you for contacting TidyDigits. We received your messsage and will be reaching out to you within 5-7 business days.<br>
+            Message you submitted:<br>
+            {submitted_message}<br><br>
+            We look forward to doing business with you!<br>
+            TidyDigits Team
+          </p>
+        </div>
+      </body>
+    </html>
+  """
+
+  email_to_admin = f"""\
+  Subject: New Submission
+
+  First Name: {first_name}
+  Last Name: {last_name}
+  Email Address: {receiver_email}
+  Message: {submitted_message}
   """
 
   # Turn these into plain/html MIMEText objects
-  part1 = MIMEText(text, "plain")
-  part2 = MIMEText(html, "html")
+  part1 = MIMEText(email_to_user_plain_text, "plain")
+  part2 = MIMEText(email_to_user_html, "html")
 
   # Add HTML/plain-text parts to MIMEMultipart message
   # The email client will try to render the last part first
@@ -69,4 +79,5 @@ def test_duplicate_user():
   with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
       server.login(sender_email, password)
       server.sendmail(sender_email, receiver_email, email_to_user.as_string())
+      server.sendmail(sender_email, sender_email, email_to_admin)
   return jsonify(success=True)
